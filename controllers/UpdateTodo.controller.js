f4525a17d6ebfe43124ec3d959c9583722619a49
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-06 17:38:52 +0530

    update todo task in dynamo db.

--- controllers/UpdateTodo.controller.js
+++ controllers/UpdateTodo.controller.js
@@ -1,27 +1,58 @@
-let database = [];
+const mapper = require("./../db/DynamoClient");
+const TodoModel = require("./../models/Todo.model");
 
-const updateTodoController = (req, res) => {
+const updateTodoController = async (req, res) => {
   // get todo id from url
   const todo_id = req.params.id;
 
-  // find index
-  const dbIndex = database.findIndex((todo) => todo.uuid === todo_id);
+  // if todo id not provided
+  if (todo_id === undefined) {
+    return res.status(400).json({
+      success: false,
+      statusCode: 400,
+      errorType: "ValidationError",
+      message: "Todo ID not provided.",
+    });
+  }
 
-  // if todo exists in database
-  if (dbIndex >= 0) {
-    // copy old todo
-    const updatedTodo = Object.assign({}, database[dbIndex]);
+  let todo = null;
 
-    // update todo status
-    updatedTodo.completed = !updatedTodo.completed;
-
-    // if new name provided for todo
-    if (req.body.name) {
-      updatedTodo.name = req.body.name;
+  try {
+    // get todo from database by id
+    todo = await mapper.get(Object.assign(new TodoModel(), { uuid: todo_id }));
+  } catch (err) {
+    if (err.name === "ItemNotFoundException") {
+      return res.status(404).json({
+        success: false,
+        statusCode: 404,
+        errorType: "DbNotFoundError",
+        message: `Todo with id : ${todo_id} not found in database.`,
+      });
     }
 
-    // update database
-    database[dbIndex] = Object.assign({}, updatedTodo);
+    // error response
+    return res.status(500).json({
+      success: false,
+      statusCode: 500,
+      errorType: "DbFindError",
+      message: `Todo with id : ${todo_id} not found in database.`,
+      error: err,
+    });
+  }
+
+  // update todo data
+  // update name
+  req.body.name !== undefined && (todo.name = req.body.name);
+
+  // update task status
+  todo.completed === true ? (todo.completed = false) : (todo.completed = true);
+
+  let updatedTodo = null;
+
+  // udpate todo data into database
+  try {
+    // update data into database
+    updatedTodo = await mapper.put({ item: todo });
 
     // return success message
     return res.status(200).json({
@@ -30,12 +61,14 @@ const updateTodoController = (req, res) => {
       message: "Todo task updated.",
       task: updatedTodo,
     });
-  } else {
-    // return error message
-    return res.status(404).json({
+  } catch (err) {
+    // return success message
+    return res.status(500).json({
       success: false,
-      statusCode: 404,
-      message: "Todo task not found in database.",
+      statusCode: 500,
+      errorType: "DbUpdateFail",
+      message: "Todo task update fail.",
+      error: err,
     });
   }
 };
