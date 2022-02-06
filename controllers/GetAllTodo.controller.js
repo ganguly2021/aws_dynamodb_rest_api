d6562ec877efa72776dd7ebad5b7e158374719b2
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-06 15:08:57 +0530

    get all todo based on complete or not.

--- controllers/GetAllTodo.controller.js
+++ controllers/GetAllTodo.controller.js
@@ -2,11 +2,36 @@ const mapper = require("./../db/DynamoClient");
 const TodoModel = require("./../models/Todo.model");
 
 const getAllTodoController = async (req, res) => {
+  // completed = 1 => not pending
+  // completed = 0 => pending
+  const completed = req.query.completed;
+
+  if (completed !== undefined) {
+    if (completed !== "1" && completed !== "0") {
+      return res.status(400).json({
+        success: false,
+        statusCode: 400,
+        errorType: "ValidationError",
+        message: "completed can be 1 or 0.",
+      });
+    }
+  }
+
   let todos = [];
 
   for await (const item of mapper.scan(TodoModel)) {
-    // add items to array
-    todos = todos.concat([{ ...item }]);
+    if (completed !== undefined) {
+      if (completed === "1") {
+        // add todos with completed = true
+        item.completed === true && (todos = todos.concat([{ ...item }]));
+      } else {
+        // add todos with completed = false
+        item.completed === false && (todos = todos.concat([{ ...item }]));
+      }
+    } else {
+      // add all todos to array
+      todos = todos.concat([{ ...item }]);
+    }
   }
 
   return res.status(200).json({
